508c386efc164f30d9b9f7b0457ac8e8621056fe
Lante_coder <allanmwangi816@gmail.com>
2025-06-27 12:47:49 +0300

    Creating a function that fetches products from API

--- src/main.js
+++ src/main.js
@@ -17,3 +17,21 @@ function startApp() {
 
     // this function is going to do some callbacks to  
 }
+
+// Get products from the Fake Store API
+function getProductsFromAPI() {
+
+    console.log('Getting products from Fake Store API...');
+    
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())  
+        .then(products => {
+            console.log('Got products:', products);
+            allProducts = products;  
+            // Create a function for the tems to display on the page and call it     
+        })
+        .catch(error => {
+            console.log('Error getting products:', error);
+            // create a function to show error message and call it 
+        });
+}
