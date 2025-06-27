413beefc4bf470e8a9e3b9e119bd807aff78bb74
Lante_coder <allanmwangi816@gmail.com>
2025-06-27 12:35:41 +0300

    initializing the js file for functionality

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// Create variables to store our data
+
+let allProducts = [];  // stores all products from the API
+let shoppingCart = [];  // stores items added to cart
+
+//When the page loads, start our app
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded! Starting One Stop Shop');
+
+    // To Call startApp function finnised writing rest of he code.
+});
+
+// Main function that starts everything
+
+function startApp() {
+
+    // this function is going to do some callbacks to  
+}
